1332bfc7cef33ca361c7b2896c687bcf45387d0c
Рита Иванова <ritaivanova@Mac.lan>
2025-08-27 13:27:37 -0400

    Fix: vite.config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: { host: true }
-})
+  plugins: [react()],
+});
